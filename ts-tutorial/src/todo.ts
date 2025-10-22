09044e872f2811639f67ea14cf85c78fd258a7e8
jasper <kai_le.aw@bp.com>
2025-10-22 13:25:00 +0800

    Update file structure

--- ts-tutorial/src/todo.ts
+++ ts-tutorial/src/todo.ts
@@ -2,26 +2,20 @@
 
 const formattedDate = formatDate(new Date());
 
-enum TodoStatus {
-      done = "Done",
-      inProgress = "in-progress",
-      todo = "todo"
-}
-
 type ContactBirthDate = Date | number | string;
 
 interface ToDoItem {
       id: number;
       title: string | number;
-      status: TodoStatus;
+      status: string; // TodoStatus;
       contactBirthDate?: ContactBirthDate;
       completedOn? : Date;
 }
 
 const todoItems: ToDoItem[] = [
-    { id: 1, title: "Learn HTML", status: TodoStatus.done, completedOn: new Date("2021-09-11") },
-    { id: 2, title: "Learn TypeScript", status: TodoStatus.inProgress },
-    { id: 3, title: "Write the best app in the world", status: TodoStatus.todo },
+    { id: 1, title: "Learn HTML", status: "done", completedOn: new Date("2021-09-11") },
+    { id: 2, title: "Learn TypeScript", status: "inProgress" },
+    { id: 3, title: "Write the best app in the world", status: "todo" },
 ]
 
 function addTodoItem(todo): ToDoItem {
