e197900e5dccdf2c50d66cf2db6e7a4ea6b3832b
jk <jayaw5508@gmail.com>
2025-10-24 16:42:36 +0800

    Update add-task length validation

--- todo-jasper/src/handler/addTaskHandler.ts
+++ todo-jasper/src/handler/addTaskHandler.ts
@@ -8,15 +8,17 @@ type inputType = undefined | string;
 const todoService = ToDoService.getInstance();
 const logger = Logger.getInstance();
 
-const logPrefixTask = "in put task";
+const logPrefixTask = "input task";
 
 export function addTaskHandle(input:HTMLInputElement, taskList: HTMLUListElement) {
       if (!input || input.value.trim() === "") {
             logger.warn(`${logPrefixTask} ${ErrorCode.EMPTYINPUT}`)
             return;
       }
-      if(input.value.trim.length > 50) {
-            logger.warn(`${logPrefixTask} ${ErrorCode.EMPTYINPUT}`)
+
+      if(input.value.length > 50) {
+            logger.warn(`${logPrefixTask} ${ErrorCode.LENGHTLIMIT}`)
+            return;
       } 
 
       const taskName = input.value.trim();
